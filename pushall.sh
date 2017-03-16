340531b70565001c10d7be19fa3f71c0d290a082
Sumit Das <sumit8das@gmail.com>
2017-03-16 07:27:26 +0530

    PUSH to GIT and BitBucket bash script

---
+++ pushall.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+git checkout master
+git push origin master
+#git checkout github/master
+git push github master
+git checkout bitbucket/master
+git push bitbucket master
+git checkout master
